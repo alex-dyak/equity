dafb9f2ee127bfefa1c89f1a26467439eaf4e6d6
alex dyakonov <alexdyakonov@3467473.no-reply.drupal.org>
2016-09-22 17:21:55 +0300

    29208 created footer

--- wp-content/themes/w4ptheme/footer.php
+++ wp-content/themes/w4ptheme/footer.php
@@ -11,6 +11,45 @@
 
 ?>
 			<footer id="footer" class="source-org vcard copyright" role="contentinfo">
+
+				<div class="join-us-footer">
+					<?php if ( is_active_sidebar( 'join-us-footer' ) ) : ?>
+						<?php dynamic_sidebar( 'join-us-footer' ); ?>
+					<?php endif; ?>
+				</div>
+
+				<!--        Navigation      -->
+				<nav id="footer-menu" role="navigation">
+					<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+				</nav>
+
+				<!--      / Navigation      -->
+
+				<div class="footer-logo">
+					<div class="footer-logo">
+						<a  target="_self" href="<?php echo get_home_url(); ?>">
+							<img class="attachment-240x89" src="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'footer_logo' ); ?>">
+						</a>
+					</div>
+				</div>
+
+				<div class="siteSidebar-footer">
+					<?php if ( is_active_sidebar( 'sidebar-footer' ) ) : ?>
+						<?php dynamic_sidebar( 'sidebar-footer' ); ?>
+					<?php endif; ?>
+				</div>
+
+				<div class="footer-social">
+					<a href="<?php echo get_option( 'w4p_social_profiles' )['twitter'][1]; ?>"
+					   target="_blank" title="Follow us on Twitter">
+						<?php _e('Twitter', 'w4ptheme'); ?>
+					</a>
+					<a href="<?php echo get_option( 'w4p_social_profiles' )['linkedin'][1]; ?>"
+					   target="_blank" title="Follow us on LinkedIn">
+						<?php _e('LinkedIn', 'w4ptheme'); ?>
+					</a>
+				</div>
+
 				<small>
 					<?php
 					if ( $copyright = get_option( 'w4p_copyright' ) ) {
@@ -23,7 +62,6 @@
 			</footer>
 
 		</div>
-
 		<?php wp_footer(); ?>
 
 	</body>
