664d88f81e4d1689d17b4d8b1dd39c6cd8e85b9f
alex dyakonov <alexdyakonov@3467473.no-reply.drupal.org>
2016-10-07 17:12:32 +0300

    29470 Create single blog post page

--- wp-content/themes/EquityX/functions.php
+++ wp-content/themes/EquityX/functions.php
@@ -183,3 +183,27 @@ function get_term_items() {
 		)
 	) );
 }
+
+/**
+ * Override get_the_excerpt
+ */
+remove_filter( 'get_the_excerpt', 'wp_trim_excerpt' );
+add_filter( 'get_the_excerpt', 'custom_trim_excerpt' );
+
+function custom_trim_excerpt( $text ) {
+	global $post;
+	if ( '' == $text ) {
+		$text           = get_the_content( '' );
+		$text           = apply_filters( 'the_content', $text );
+		$text           = str_replace( ']]>', ']]>', $text );
+		$text           = strip_tags( $text );
+		$excerpt_length = 50;
+		$words          = explode( ' ', $text, $excerpt_length + 1 );
+		if ( count( $words ) > $excerpt_length ) {
+			array_pop( $words );
+			$text = implode( ' ', $words );
+		}
+	}
+
+	return $text;
+}
--- wp-content/themes/EquityX/single.php
+++ wp-content/themes/EquityX/single.php
@@ -2,15 +2,27 @@
 /**
  * The template for displaying all single posts and attachments
  *
- * @package WordPress
+ * @package    WordPress
  * @subpackage W4P-Theme
- * @since W4P Theme 1.0
+ * @since      W4P Theme 1.0
  */
 
 get_header(); ?>
 
+<?php
+$image = get_field( 'background_image' );
+
+if ( ! empty( $image ) ): ?>
+	<div>
+		<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>"/>
+	</div>
+<?php endif; ?>
+
+
 <?php if ( have_posts() ) :
 	while ( have_posts() ) : the_post(); ?>
+
+		<a href="<?php echo get_post_type_archive_link( 'post' ); ?>"><?php echo __( strtoupper( 'Back to main' ), 'EquityX' ); ?></a>
 
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 
@@ -18,25 +30,44 @@ get_header(); ?>
 
 			<div class="entry-content">
 
-				<?php the_content(); ?>
+				<div>
+					<?php _e( 'by ', 'EquityX' ); ?>
+					<?php echo get_the_author_posts_link(); ?>    &#8212;
+					<?php printf( _x( '%s ago', '%s = human-readable time difference', 'EquityX' ),
+						human_time_diff( get_the_time( 'U' ), current_time( 'timestamp' ) ) ); ?>
+				</div>
 
-				<?php wp_link_pages( array( 'before' => __( 'Pages: ', 'EquityX' ), 'next_or_number' => 'number' ) ); ?>
+				<div>
+					<?php the_excerpt(); ?>
+				</div>
 
-				<?php the_tags( __( 'Tags: ', 'EquityX' ), ', ', '' ); ?>
+				<div>
+					<?php the_post_thumbnail(); ?>
+				</div>
+
+				<div>
+					<?php _e( 'Photo: ', 'EquityX' ); ?>
+					<?php echo get_the_author_posts_link(); ?>    &#8212;
+					<?php echo date( "d F Y" ); ?>
+				</div>
+
+				<div>
+					<?php the_content(); ?>
+				</div>
 
 			</div>
 
-			<?php edit_post_link( __( 'Edit this entry', 'EquityX' ), '', '.' ); ?>
-
 		</article>
-
-		<?php comments_template(); ?>
 
 	<?php endwhile;
 endif; ?>
 
-<?php post_navigation(); ?>
-
 <?php get_sidebar(); ?>
+
+<div class="join-us-footer">
+	<?php if ( is_active_sidebar( 'join-us-footer' ) ) : ?>
+		<?php dynamic_sidebar( 'join-us-footer' ); ?>
+	<?php endif; ?>
+</div>
 
 <?php get_footer(); ?>
