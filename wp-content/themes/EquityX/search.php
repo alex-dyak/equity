61f26613159012ab9c0c22b0c4a6f35e3f2fcc91
alex dyakonov <alexdyakonov@3467473.no-reply.drupal.org>
2016-10-10 15:28:15 +0300

    29475 Change path to template part in the search page.

--- wp-content/themes/EquityX/search.php
+++ wp-content/themes/EquityX/search.php
@@ -15,7 +15,7 @@ get_header(); ?>
 
 	<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php get_template_part( 'template-article-blog-list' ); ?>
+		<?php get_template_part( 'templates-part/template-article-blog-list' ); ?>
 
 	<?php endwhile; ?>
 
