c4546aaba6423d1a158a6afac55c87a9f809d795
Viktor_RyuKit <vsichevoy@corp.web4pro.com.ua>
2016-10-17 17:35:48 +0300

    29565 styles and layout for 404page

--- wp-content/themes/EquityX/404.php
+++ wp-content/themes/EquityX/404.php
@@ -9,8 +9,23 @@
 
 get_header(); ?>
 
-<h2><?php esc_html_e( 'Error 404 - Page Not Found', 'EquityX' ); ?></h2>
+<div class="parallaxHolder">
+    <?php
+    $image = get_option( 'w4p_404_background_image' );
 
-<?php get_sidebar(); ?>
+    if ( ! empty( $image ) ): ?>
+        <div class="parallaxHolder-item" data-parallax="scroll" data-image-src="<?php echo $image['url']; ?>"></div>
+    <?php endif; ?>
+</div> <!-- Parallax section -->
+
+<div class="main defaultPage defaultPage--short"> <!-- Start main container -->
+    <div class="u-clearfix container">
+        <div class="notFoundContainer">
+            <h1><?php esc_html_e( 'Error 404', 'EquityX' ); ?></h1>
+            <p class="notFoundContainer-mainText"><?php esc_html_e( 'We couldn’t find what are you looking for…', 'EquityX' ); ?></p>
+            <p class="notFoundContainer-redirectText"><?php esc_html_e( 'Try visiting our', 'EquityX' ); ?>
+                <a href="<?php echo get_home_url(); ?>"><?php esc_html_e( 'home page', 'EquityX' ); ?></a> <?php esc_html_e( 'or', 'EquityX' ); ?>
+                <a href="<?php echo get_page_link(15); ?>"><?php esc_html_e( 'contact us', 'EquityX' ); ?></a> <?php esc_html_e( 'if it keeps happening.', 'EquityX' ); ?></p>
+        </div>
 
 <?php get_footer(); ?>
