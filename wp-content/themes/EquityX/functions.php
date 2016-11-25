c4cc2a484a75e8fe377b1c1e7d22502e7da2295d
alex dyakonov <alexdyakonov@3467473.no-reply.drupal.org>
2016-11-25 15:03:45 +0200

    30131 Delete custom taxonomy

--- wp-content/themes/EquityX/functions.php
+++ wp-content/themes/EquityX/functions.php
@@ -205,11 +205,6 @@ add_action( 'vc_before_init', 'get_members' );
 function get_members() {
 	$args = array(
 		'type'      => 'member',
-		'tax_query' => array(
-			array(
-				'taxonomy' => 'team-group',
-			)
-		)
 	);
 	$categories = get_categories( $args );
 	$cat_name = array();
