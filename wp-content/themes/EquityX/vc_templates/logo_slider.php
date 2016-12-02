ce32138047b69ac78585a79d420df95566ca443a
alex dyakonov <alexdyakonov@3467473.no-reply.drupal.org>
2016-12-02 11:26:53 +0200

    30188 Change logo slider

--- wp-content/themes/EquityX/vc_templates/logo_slider.php
+++ wp-content/themes/EquityX/vc_templates/logo_slider.php
@@ -6,7 +6,8 @@ $speed = $autoplay_speed * 1000;
 if ( ! empty( $quantity ) ) :
 	$query_args = array(
 		'post_type'      => 'clients-logo',
-		'posts_per_page' => - 1
+		'posts_per_page' => $quantity,
+		'paged'          => 1,
 	);
 	$query      = new WP_Query( $query_args );
 	?>
@@ -24,18 +25,24 @@ if ( ! empty( $quantity ) ) :
 	"slidesToScroll": <?php echo $slides_to_scroll; ?>,
 	"autoplaySpeed": <?php echo $speed; ?> }'>
 		<?php
-		if ( $query->have_posts() ) : ?>
-			<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-				<div class="slide-item">
-					<?php if ( has_post_thumbnail() ) : ?>
-						<div class="logo-image">
-							<?php the_post_thumbnail(); ?>
-						</div>
-					<?php endif; ?>
-				</div>
-			<?php endwhile; ?>
-		<?php endif; ?>
+		if ( $query->have_posts() ) : while ( $query->have_posts() ) :?>
+			<div class="slide-container">
+				<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+					<div class="slide-item">
+						<?php if ( ! empty ( get_field( 'image_clients_logo' ) ) ) : ?>
+							<div class="logo-image">
+								<img src="<?php echo get_field( 'image_clients_logo' ); ?>" alt="">
+							</div>
+						<?php endif; ?>
+					</div>
+				<?php endwhile; ?>
+				<?php wp_reset_postdata(); ?>
+				<?php
+				$query_args['paged'] ++;
+				$query = new WP_Query( $query_args );
+				?>
+			</div>
+		<?php endwhile; ?>
+		<?php endif ?>
 	</div>
 <?php endif;
-// Restore original Post Data
-wp_reset_postdata();
