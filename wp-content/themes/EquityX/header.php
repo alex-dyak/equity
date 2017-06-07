e1fac6bda70220258c09c084be463f84134d68df
Viktor_RyuKit <vsichevoy@corp.web4pro.com.ua>
2017-06-07 14:14:31 +0300

    added link title fixes

--- wp-content/themes/EquityX/header.php
+++ wp-content/themes/EquityX/header.php
@@ -122,7 +122,7 @@
 					</a>
 				<?php endif; ?>
                 <?php if ( $expert_link ) : ?>
-                <a href="<?php echo $expert_link; ?>" class="btn btn--login" title="<?php _e( 'Become an Expert', 'EquityX' ); ?>">
+                <a href="<?php echo $expert_link; ?>" class="btn btn--login" title="<?php _e( 'Join as an Expert', 'EquityX' ); ?>">
                     <?php _e( 'Become an Expert', 'EquityX' ); ?>
                 </a>
                 <?php endif; ?>
